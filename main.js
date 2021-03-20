9f575fefafe0f89e77b50c0b3b36c62b120b83ad
svirid132 <svirid132@gmail.com>
2021-03-20 12:18:41 +0300

    add moving pingvin

--- main.js
+++ main.js
@@ -1,116 +1,192 @@
 let menu = document.querySelector('.Menu .play');
 menu.addEventListener('click', initGame);
 
-class managerImageSizes {
+class managerSpriteCoordinate {
     #widthItemSprite = 0;
     #heightItemSprite = 0;
+    #indexRow = 0;
+    #indexColumn = 0;
+    #direction = {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+    };
 
     constructor (widthItemSprite, heightItemSprite) {
         this.#widthItemSprite = widthItemSprite;
         this.#heightItemSprite = heightItemSprite;
     }
 
-    getXSprite(indexRow) {
-        return indexRow * this.#widthItemSprite;
+    setDirection(direc) { //left, right, up, down
+        let newArrDir = Object.entries(direc);
+        let currentArrDir = Object.entries(this.#direction);
+
+        
     }
 
-    getYSpite(indexColumn) {
-        return indexColumn * this.#heightItemSprite;
+    getCoordinate() {
+        return (
+            {
+                x: this.#indexColumn * this.#widthItemSprite, 
+                y: this.#indexRow * this.#heightItemSprite, 
+            });
     }
 };
 
 class Person {
-    #x = 0;
-    #y = 0;
-    #img;
-    #managerImage;
-
-    //count Pixel moving
-    #right = 0;
-    #left = 0;
-    #jumpUp = 0;
-    #jumpDown = 0;
-
-    constructor(x, y) {
-        this.#x = x;
-        this.#y = y;
-    }
-
-    jump() {
-        if (!this.#jumpUp && !this.#jumpDown) {
-            this.#jumpUp = 10;
-            this.#jumpDown = 10;
-        }
-    }
-
-    moveLeft() {
-        if (this.#left === 0) this.#left = 4;
-    }
-
-    moveRight() {
-        if (this.#right === 0) this.#right = 4;
-    }
-
-    setImage(imgPath, itemWidth, itemHeight) {
-        let imgPerson = new Image();
-        imgPerson.src = imgPath;
-        this.#managerImage = new managerImageSizes(itemWidth, itemHeight);
-        this.#img = imgPerson;
+    #coordinate = {
+        x: 0,
+        y: 0,
+    };
+    #move = {
+        right: false,
+        left: false,
+        up: false,
+        down: false,
+        speedStep: 50,
+        speedJump: 170,
+        onUp: 0,
+    };
+    #sprite;
+    #spriteCoordinate;
+    #spriteProps = {
+        speedChange: 7,
+        indexRow: 0,
+        indexColumn: 0,
+        dChangeColumn: 0,
+        countItem: 0,
     };
 
-    getImage() {
-        return this.#img;
+    constructor(x, y) {
+        this.#coordinate.x = x;
+        this.#coordinate.y = y;
     }
 
-    getX() {
-        if (this.#left !== 0) {
-            this.#left -= 1;
-            this.#x -= 1;
-        } else if (this.#right !== 0) {
-            this.#right -= 1;
-            this.#x += 1;
+    setMove(direction) {
+        switch (direction) {
+            case 'up':
+                if (this.isJump()) return;
+                this.#move.up = true;
+                this.#move.onUp = 60;
+                break;
+            case 'left':
+                if (this.#move.right === true) return;
+                this.#move.left = true;
+                break;
+            case 'right':
+                if (this.#move.left === true) return;
+                this.#move.right = true;
+                break;
         }
-        return this.#x;
     }
 
-    getY() {
-        if (this.#jumpUp !== 0) {
-            this.#jumpUp -= 1;
-            this.#y -= 1;
-        } else if (this.#jumpDown !== 0) {
-            this.#jumpDown -= 1;
-            this.#y += 1;
-        }
-        return this.#y;
+    resertSpriteColumn() {
+        this.#spriteProps.dChangeColumn = 0;
+        this.#spriteProps.indexColumn = 0;
     }
 
-    getXSpriteСoordinate () {
-
-        if (this.#jumpUp > 0) {
-            return this.#managerImage.getXSprite(1);
-        } else if (this.#jumpDown > 0) {
-            return this.#managerImage.getXSprite(2);
-        }
-
-        switch (this.#right) {
-            case 4:
-                return this.#managerImage.getXSprite(1);
-            case 3:
-            case 2:
-                return this.#managerImage.getXSprite(2);
-            case 3:
-                return this.#managerImage.getXSprite(3);
-        }
-
-        return this.#managerImage.getXSprite(0);
+    isJump() {
+        return this.#move.up || this.#move.down;
     }
 
-    getYSpriteСoordinate () {
-        if (this.#jumpUp > 0 || this.#jumpDown > 0) {
-            return this.#managerImage.getYSpite(1);
+    setUnmove(direction) {
+        switch (direction) {
+            case 'left':
+                this.#move.left = false;
+                if (!this.isJump()) this.resertSpriteColumn();
+                break;
+            case 'right':
+                this.#move.right = false;
+                if (!this.isJump()) this.resertSpriteColumn();
+                break;
+        }
+    }
+
+    setSprite(path, itemWidth, itemHeight) {
+        this.#sprite = new Image();
+        this.#sprite.src = path;
+        this.#spriteCoordinate = new managerSpriteCoordinate(itemWidth, itemHeight);
+    }
+
+    getSpriteImage() {
+        return this.#sprite;
+    }
+
+    getCoordinate() {
+        return this.#coordinate;
+    }
+
+    getSpriteCoordinate() {
+        const jupm = isJump();
+        const row = (this.#move.right === true && jump ? 2 : 
+                    this.#move.right === true && !jump ? 0 :
+                    this.#move.left === true && jump ? 3 : 1);
+        const 
+        if(this.isJump()) {
+            const row = (this.#move.right === true);
+            if ()
         } else {
-            return this.#managerImage.getYSpite(0);
+
         }
+
+        if (this.#move.up && this.#move.right) {
+            return this.#spriteCoordinate.getCoordinate(2, 1);
+        } else if (this.#move.down) {
+            return this.#spriteCoordinate.getCoordinate(1, 2);
+        } else if (this.#move.left || this.#move.right) {
+            return this.#spriteCoordinate.getCoordinate(0, this.#spriteProps.indexColumn);
+        }
+        return this.#spriteCoordinate.getCoordinate(0, 0);
+    }
+
+    updateCoordinate(dt) {
+        if (this.#move.left === true) {
+            this.#coordinate.x -= this.#move.speedStep * dt;
+        }
+        if (this.#move.right === true) {
+            this.#coordinate.x += this.#move.speedStep * dt;
+        }
+        if (this.#move.up === true) {
+            const dChange = this.#move.speedJump * dt;
+            this.#move.onUp -= dChange;
+            console.log(this.#move.onUp);
+            if (Math.floor(this.#move.onUp) < 0) {
+                this.#move.up = false;
+                this.#move.down = true;
+                return;
+            }
+            this.#coordinate.y -= dChange;
+        }
+        if (this.#move.down === true) {
+            this.#coordinate.y += this.#move.speedJump * dt;
+        }
+    }
+
+    updateCoordinateSprite(dt) {
+        if (this.#move.right || this.#move.left && !this.isJump()) {
+            this.#spriteProps.indexRow = 0;
+            this.#spriteProps.dChangeColumn += dt * this.#spriteProps.speedChange;
+            const column = Math.floor(this.#spriteProps.dChangeColumn);
+            if (column > 3) { 
+                this.resertSpriteColumn();
+                return;
+            }
+            this.#spriteProps.indexColumn = column;
+        }
+        if (this.isJump()) {
+            this.#spriteProps.indexRow = 1;
+            if (this.onUp > 0) {
+                this.#spriteProps.indexColumn = 1;
+            } else {
+                this.#spriteProps.indexColumn = 2;
+            }
+        }
+    }
+
+    update(dt) {
+        this.updateCoordinate(dt);
+        this.updateCoordinateSprite(dt);
     }
 };
 
@@ -136,9 +212,16 @@ const Y_BEGIN_FOR_PERSON = HEIGHT_FIELD - HEIGHT_PLATFORM - 40;
 // const Y_BEGIN_FOR_PERSON = 0;
 
 let person = new Person(X_BEGIN_FOR_PERSON, Y_BEGIN_FOR_PERSON, WIDTH_PERSON, HEIGHT_PERSON);
-person.setImage('screen/pingvins.png', WIDTH_PERSON, HEIGHT_PERSON);
+person.setSprite('screen/pingvins.png', WIDTH_PERSON, HEIGHT_PERSON);
+
+let leterTime;
+let needPerson = 100;
 
 function playGame() {
+
+    let now = Date.now();
+    let dt = (now - leterTime) / 1000.0;
+
     let x = 10;
     let y = HEIGHT_FIELD - HEIGHT_PLATFORM - 43;
 
@@ -156,10 +239,17 @@ function playGame() {
             WIDTH_PLATFORM, HEIGHT_PLATFORM);
     }
 
-    ctx.drawImage(person.getImage(), 
-        person.getXSpriteСoordinate(), person.getYSpriteСoordinate(), 
+    person.update(dt);
+    const spriteCoordinate = person.getSpriteCoordinate();
+    const coordinate = person.getCoordinate();
+
+    // console.log(`coordinate: ${coordinate.x} ${coordinate.y}`);
+    // console.log(`spriteCoordinate: ${spriteCoordinate.x} ${spriteCoordinate.y}`);
+
+    ctx.drawImage(person.getSpriteImage(), 
+        spriteCoordinate.x, spriteCoordinate.y, 
         WIDTH_PERSON, HEIGHT_PERSON, 
-        person.getX(), person.getY(), 
+        coordinate.x, coordinate.y, 
         WIDTH_PERSON, HEIGHT_PERSON);
 
 
@@ -177,6 +267,8 @@ function playGame() {
 
     if (currentIndexFrame === 3) currentIndexFrame = 0; 
     ++currentIndexFrame;
+
+    leterTime = now; 
 }
 
 let canvas = document.querySelector('.GameCanvas');
@@ -185,22 +277,32 @@ var ctx = canvas.getContext("2d");
 let imgPlatform = new Image();
 imgPlatform.src = 'screen/Platforms.png';
 
-setInterval(playGame, 25);
+letterTime = Date.now();
+
+setInterval(playGame, 1000 / 60);
 
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 document.addEventListener('keydown', function(event) {
         if (event.code == 'KeyD') {
-            console.log(event.code);
-            person.moveRight();
+            person.setMove('right');
         } else if (event.code == 'KeyA') {
-            person.moveLeft();
+            person.setMove('left');
         }   
         if (event.code == 'Space') {
-            console.log(event.code);
-            person.jump();
+            person.setMove('up')
         }
     }
+);
+
+document.addEventListener('keyup', function(event) {
+        if (event.code == 'KeyD') {
+            person.setUnmove('right');
+        } else if (event.code == 'KeyA') {
+            person.setUnmove('left');
+        } 
+    }   
 );
